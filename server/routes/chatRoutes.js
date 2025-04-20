d4eb47806017183bf6d04016d2c7312e6675693c
Dmitrij Mayzer <dima.kondrashin.2003@gmail.com>
2025-04-21 01:15:26 +0300

    эх

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/authMiddleware');
+
 
 // Получить все сообщения между репетитором и учеником
 router.get('/', async (req, res) => {
